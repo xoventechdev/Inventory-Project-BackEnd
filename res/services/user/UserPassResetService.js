06b835212edaac945faf13de662c21cf4b77bf18
xoventechdev <alkarim.doha@gmail.com>
2024-07-13 20:01:06 +0300

    password recover : done

--- res/services/user/UserPassResetService.js
+++ res/services/user/UserPassResetService.js
@@ -3,12 +3,12 @@ import bcrypt from "bcrypt";
 
 export const UserPassResetService = async (req, model) => {
   try {
-    let { email, otp, newPass } = req.body;
+    let { email, otp, password} = req.body;
     let usedOTP = await OTPModel.aggregate([
       { $match: { email: email, otp: otp, status: 1 } },
     ]);
     if (usedOTP.length > 0) {
-      let hash = await bcrypt.hash(newPass, 10);
+      let hash = await bcrypt.hash(password, 10);
       await model.findOneAndUpdate({ email }, { password: hash });
 
       return { status: "success", response: "Password reset successfully" };
